43bc2fb7ec7b5504e5b46df8980c1535bd974123
Frank Ramirez <89478677+fjramirezg@users.noreply.github.com>
2022-06-27 20:16:07 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./Styles.css";
+import quotes from "./quotes.json";
+import colors from "./colors";
+
+// parte funcional
+
+function App() {
+  // seleccion al azar
+  const random = Math.floor(Math.random() * quotes.length);
+  const [index, setIndex] = useState(random);
+
+  //cambio al azar
+  const changeUser = () => {
+    const random = Math.floor(Math.random() * quotes.length);
+    setIndex(random);
+  };
+  //color al azar
+  const randomColorIndex = Math.floor(Math.random() * colors.length);
+  const color = colors[randomColorIndex];
+
+  document.body.style = `background: ${colors[index].hex}`;
+
+  //parte visual
+
+  return (
+    <div className="container">
+      <div className="card " style={{ color: "black" }}>
+        <h1>
+          <i className="fa-solid fa-quote-left"></i> {quotes[index].quote}
+        </h1>
+        <div className="footer">
+          <h2>
+            {`"`}
+            {quotes[index].author}
+            {`"`}
+          </h2>
+          <button onClick={changeUser}>
+            <i class="fa-solid fa-circle-arrow-right"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
